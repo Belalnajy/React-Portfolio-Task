9d526c082a5770ec54c6f350e15dcf87750c699d
belalnagy <belalnajy9@gmail.com>
2025-03-14 16:41:00 +0200

    edit about section

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -11,19 +11,21 @@ const About = () => {
         <h2 className="text-center mb-5 fw-bold about-text ">About Me</h2>
 
         <div className="col-lg-8 col-md-7">
-          <h3 className="mb-3">I'm John Doe</h3>
-          <h5 className="text-muted mb-4">Frontend Developer & Designer</h5>
+          <h3 className="mb-3">I'm Belal Nagy</h3>
+          <h5 className="text-muted mb-4">Software Engineer</h5>
 
           <p className="lead">
-            I'm a passionate developer with over 5 years of experience in
-            creating beautiful and functional web applications. I specialize in
-            crafting user-friendly interfaces and bringing designs to life.
+            Motivated and dedicated Software Engineer with expertise in Python
+            and Django. Passionate about developing scalable web applications
+            and enhancing user experiences. Eager to apply my knowledge in a
+            dynamic and collaborative work environment. Committed to continuous
+            learning and professional growth in the tech industry.
           </p>
         </div>
         <a
           href="../../../public/Belal-Nagy-CV.pdf"
           download="Belal-Nagy-CV.pdf"
-          className="btn btn-primary mt-4">
+          className="btn btn-primary mt-4 p-3">
           Download CV
         </a>
       </div>
